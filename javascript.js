5a9adb5bfd4ac268f8289c9ab9e65ade7e9707c5
Alexander Gorham <agram@hotmail.co.uk>
2025-05-26 22:25:52 +0200

    SIngle page build out

---
+++ javascript.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () =>
+{
+    //Navbar
+    const navbar = document.getElementById("navbar");
+
+    //Hero
+    const heroSection = document.getElementById("hero-section");
+
+    //
+    console.log("Hello world!");
+})
